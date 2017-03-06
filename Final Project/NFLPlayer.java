fbdb57a01bf5e641dfdfadb8f757038d4114cb21
COOKIEMOUNTAIN\Kimberly Moore <kmmoore789@gmail.com>
2017-03-05 22:55:18 -0800

    no message

---
+++ Final Project/NFLPlayer.java
@@ -0,0 +1,28 @@
+package final_project;
+
+public class NFLPlayer {
+
+	public static void main(String[] args) {
+		/* Fully implement a class, making use of at least two variable types to define
+		   its properties
+		 * Create the NFLPlayer class and add its properties
+		 * Include player statistics found on the NFL website
+		 * Use the variable types learned so far to create the player properties */
+		
+		NFLPlayer player = new NFLPlayer();
+
+	}
+
+}
+
+	class NFLPlayer {
+		long passingYards;
+		long rushingYards;
+		long receivingYards;
+		int tackles;
+		int sacks;
+		int interceptions;
+		int tackles;
+		String name;
+		
+	}
