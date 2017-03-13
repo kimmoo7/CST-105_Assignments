7396e9be7840a3fd4f651b8a3b3ffa88dd652e3a
COOKIEMOUNTAIN\Kimberly Moore <kmmoore789@gmail.com>
2017-03-12 23:55:16 -0700

    no message

---
+++ Topic 3/TimeConversion/Part_One.java
@@ -0,0 +1,22 @@
+package timeConversion;
+
+import java.util.Scanner;
+
+public class Part_One {
+
+	public static void main(String[] args) {
+		/* Write a program that will prompt the user to enter an hour in the 24-hour 
+		   format and print it in the 12-hour format. Example: 18 prints 6pm
+		 * Convert data through the use of a formatter class and the use of upcasting 
+		   and downcasting.
+		 * Read information entered by user, format it, and print in the console. */
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an hour in the 24-hour format: ");
+		int twentyFourHour = input.nextInt();
+
+	}
+	
+	public static int doConversion(int twentyFourHour) {
+		
+	}
---
+++ Topic 3/TimeConversion/Part_Two.java
@@ -0,0 +1,21 @@
+package timeConversion;
+
+import java.util.Scanner;
+
+public class Part_Two {
+
+	public static void main(String[] args) {
+		/* Write a program that will prompt the user to enter a time in a decimal format 
+		   and print the amount of hours and minutes from the value entered. Example, 
+		   user enters 4.5 and application parses to 4 hours and 30 minutes.
+		 * Convert data through the use of a formatter class and the use of upcasting 
+		   and downcasting.
+		 * Read information entered by user, format it and print in the console */
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a time in decimal format: ");
+		int decimal = input.nextInt();
+	}
+	
+
+}
