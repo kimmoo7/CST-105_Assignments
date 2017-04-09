1a4baf72b1d559e105654a0c7ed96e4086909853
kimmoo7 <kmmoore789@gmail.com>
2017-04-08 20:25:54 -0700

    Update Exercise_Thirteen.java

--- Topic 6/Chapter 13/Exercise_Thirteen.java
+++ Topic 6/Chapter 13/Exercise_Thirteen.java
@@ -3,8 +3,18 @@ package chapter13;
 public class Exercise_Thirteen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Rewrite the Course class in Listing 10.6 to add a clone method to perform a 
+		   deep copy on the students field */
 
+	      Course course1 = new Course("Course1");
+	        course1.addStudent("student1");
+	        course1.addStudent("student2");
+	        Course course2 = (Course)course1.clone();
+	        System.out.println(course1 == course2);
+	        System.out.println(course1.getCourseName() == course2.getCourseName());
+	        System.out.println(course1.getStudents() == course2.getStudents());
+	
 	}
+
 
 }
