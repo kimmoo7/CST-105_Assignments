14e5351265c63941e2951bd10a3bb5b416f15dbe
kimmoo7 <kmmoore789@gmail.com>
2017-04-08 20:21:42 -0700

    Update DefensivePlayer.java

--- Final Project/DefensivePlayer.java
+++ Final Project/DefensivePlayer.java
@@ -1,14 +1,15 @@
 package final_project;
 
-public class DefensivePlayer extends NFLPlayer {
+public class DefensivePlayer extends NFLPlayer implements Celebrator {
 
 	private int tackles;
 	private int sacks;
 	private int interceptions;
 	
-	DefensivePlayer(String name, int tackles, int sacks, int interceptions) {
+	DefensivePlayer(String team, String name, 
+			String position, int tackles, int sacks, int interceptions) {
 			
-		super(name);
+		super(team, name, position);
 		
 		this.tackles = tackles;
 		this.sacks = sacks;
@@ -38,6 +39,16 @@ public class DefensivePlayer extends NFLPlayer {
 	
 	public void setInterceptions(int interceptions) {
 		this.interceptions = interceptions;
+	}
+	
+	public void celebrate() {
+		String [] celebrations = {this.name + " eats a cake", 
+		                          this.name + " dances ballet solo", 
+		                          this.name + " sings \"All I do is Win\"", 
+		                          this.name + " does awesome break dancing moves",
+		                          this.name + " buys a monster slurpee"};
+		int random = (int) (Math.random() * 5);
+		System.out.println(celebrations[random] + " to celebrate his draft");
 	}
 	
 }
